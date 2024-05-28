49af74170368fffcac9ceacf382ba9d5c758cc29
sahilaf <sahilfarib320@gmail.com>
2024-05-28 20:55:06 +0600

    second

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+    domains: ['aceternity.com', 'www.google.com'],
+
+  },
   output:'export',
   distDir: 'out',
 };
