a1fd319574ad1a8e5978e4af42ad6837804b0b1b
sahilaf <sahilfarib320@gmail.com>
2024-06-25 21:39:21 +0600

    he

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dr. Shaidul Houqe",
-  description: "Dr. Shaidul Houqe's Portfolio Website",
+  description: "Experienced dental surgeon dedicated to exceptional oral care. Offering comprehensive services including general, cosmetic, restorative, implant dentistry, and oral surgery. Patient-centered approach for comfort and satisfaction.",
 };
 
 export default function RootLayout({
@@ -20,6 +20,12 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* Open Graph meta tags for the feature picture */}
+        <meta property="og:title" content="Dr. Shaidul Houqe" />
+        <meta property="og:description" content="Experienced dental surgeon dedicated to exceptional oral care. Offering comprehensive services including general, cosmetic, restorative, implant dentistry, and oral surgery. Patient-centered approach for comfort and satisfaction." />
+        <meta property="og:image" content="/gird4.jpg" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://site-dun-one.vercel.app/" /> {/* Replace with your actual URL */}
       </head>
       <body className={inter.className}>
         <ThemeProvider
